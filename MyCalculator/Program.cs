797c2a6d771b3a9ddbbce18b081fd6c4a77d00a4
Sarah Gilbreath <thenoiseinspace@gmail.com>
2023-02-02 15:32:23 -0500

    adding error for numbers outside range

--- MyCalculator/Program.cs
+++ MyCalculator/Program.cs
@@ -12,20 +12,36 @@ namespace MyCalculator
             double secondDouble = 0;
             double totalDouble = 0;
             bool OKBool = true;
-            Console.WriteLine("Welcome to My Calculator.");
-
-            Console.Write("\nEnter first number: ");
+            Console.WriteLine("Welcome to my Calculator");
+            Console.Write("\nEnter first number ");
             firstDouble = double.Parse(Console.ReadLine());
+            if ((firstDouble < _MIN_VALUE_Double) || (firstDouble > _MAX_VALUE_Double))
+            {
+                Console.WriteLine("\nFirst input number, {0}, is not within range of {1} and {2}. Program will end.\n",
+                    firstDouble, _MIN_VALUE_Double, _MAX_VALUE_Double);
+                OKBool = false;
+            }
+            if (OKBool)
+            {
+                Console.Write("\nEnter second number: ");
+                secondDouble = double.Parse(Console.ReadLine());
 
-            Console.Write("\nEnter second number: ");
-            secondDouble = double.Parse(Console.ReadLine());
+                if ((secondDouble < _MIN_VALUE_Double) || (secondDouble > _MAX_VALUE_Double))
+                {
+                    Console.WriteLine("\nSecond input number, {0}, is not within range of {1} and {2}. Program will end.\n",
+                        secondDouble, _MIN_VALUE_Double, _MAX_VALUE_Double);
+                    OKBool = false;
+                }
+            }
+            if (OKBool)
+            {
+                totalDouble = firstDouble + secondDouble;
 
-            totalDouble = firstDouble + secondDouble;
+                Console.WriteLine("\n{0} + {1} = {2}\n",
+                    firstDouble, secondDouble, totalDouble);
+            }
 
-            Console.WriteLine("\n{0} + {1} = {2}\n",
-                firstDouble, secondDouble, totalDouble);
-
-            Console.Write("Press ENTER key to end program.");
+            Console.WriteLine("Press ENTER key to end program");
             Console.ReadLine();
         }
     }
